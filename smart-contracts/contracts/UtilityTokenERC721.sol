6b59d515b3d43a57b282bce597fd3c492b8ecd77
Kamal <kamalproacc@gmail.com>
2022-05-17 00:58:10 +0530

    deleted some unnecessary files

--- smart-contracts/contracts/UtilityTokenERC721.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.0;
-
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract UtilityTokenERC721 is ERC721 {
-    constructor() ERC721("UniLoanTokenSEC721", "ULTERC721") {}
-}
