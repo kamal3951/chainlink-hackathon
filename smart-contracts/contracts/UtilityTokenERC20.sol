65c445888c9cea0e8db807b2eec41194ed2ee097
Kamal <kamalproacc@gmail.com>
2022-05-17 00:43:40 +0530

    installed uniswapV3 periphery contracts

--- smart-contracts/contracts/UtilityTokenERC20.sol
+++ smart-contracts/contracts/UtilityTokenERC20.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract UtilityTokenERC20 is ERC20 {
     constructor() ERC20("UniLoanTokenERC20", "ULTERC20") {
--- smart-contracts/contracts/p2p.sol
+++ smart-contracts/contracts/p2p.sol
@@ -1,17 +1,20 @@
 //SPDX-License-Identifier:MIT
 pragma solidity >0.8.4;
 
-import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "../node_modules/@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 error p2p__TransferFailed();
 
 contract p2p is ReentrancyGuard {
     //Token allowed to stake
-    IERC721 public listingToken;
+    IERC721 public immutable listingToken;
     //ERC20 allowed to lend
-    IERC20 public lendingMoney;
+    IERC20 public immutable lendingMoney;
 
     //struct listing
     struct listing {
@@ -42,9 +45,42 @@ contract p2p is ReentrancyGuard {
 
     //modifier updateReward(address account) {}
 
-    constructor() {
-        listingToken = IERC721("0xC36442b4a4522E871399CD717aBDD847Ab11FE88"); //UniswapV3 Rinkeby Adddress
-        lendingMoney = IERC20("RINKEBY-DAI-ADDRESS"); //Rinkeby DAI Address
+    constructor(address _listingTokenAddress, address _lendingMoneyAddress) {
+        //listingToken = IERC721("0xC36442b4a4522E871399CD717aBDD847Ab11FE88"); //UniswapV3 Rinkeby Adddress
+        listingToken = IERC721(_listingTokenAddress);
+        lendingMoney = IERC20(_lendingMoneyAddress); //Rinkeby DAI Address
+    }
+
+    function listNft(
+        uint256 tokenId,
+        uint256 LoanAmount,
+        uint256 LoanTimePeriod
+    ) external returns (bool) {
+        //fetch the liquidity and set amount < 0.5 * liquidity
+        uint256 NftLiquidity;
+        listingToken.position(tokenId) = (, , , , , , , NftLiquidity, , , , );
+        require(
+            LoanAmount < NftLiquidity / 2,
+            "You can not get loan of more than 50% worth of your position's liquidity"
+        );
+        listing storage Listing = listing(
+            msg.sender,
+            tokenId,
+            LoanAmount,
+            LoanTimePeriod
+        );
+        bool success = listingToken[tokenId].safeTransferFrom(
+            msg.sender,
+            address(this),
+            tokenId
+        );
+
+        listerAddressToListedStruct[msg.sender].push(Listing);
+
+        totalTokenSupplyWorth += uint256(NftLiquidity);
+        if (!success) {
+            revert p2p__TransferFailed();
+        }
     }
 
     function lendMoney(listing memory Listing) external returns (bool) {
@@ -79,38 +115,9 @@ contract p2p is ReentrancyGuard {
         }
     }*/
 
-    function listNft(
-        uint256 tokenId,
-        uint256 LoanAmount,
-        uint256 LoanTimePeriod
-    ) external returns (bool) {
-        //fetch the liquidity and set amount < 0.5 * liquidity
-        uint256 NftLiquidity;
-        listingToken.position(tokenId) = (, , , , , , , NftLiquidity, , , , );
-        require(
-            LoanAmount < NftLiquidity / 2,
-            "You can not get loan of more than 50% worth of your position's liquidity"
-        );
-        listing Listing = listing(
-            msg.sender,
-            tokenId,
-            LoanAmount,
-            LoanTimePeriod
-        );
-        bool success = listingToken[tokenId].safeTransferFrom(
-            msg.sender,
-            address(this),
-            tokenId
-        );
+    function unstakeNFT() external returns (bool) {}
 
-        listerAddressToListedStruct[msg.sender].push(Listing);
-
-        totalTokenSupplyWorth += uint256(NftLiquidity);
-        if (!success) {
-            revert p2p__TransferFailed();
-        }
-    }
-
+    //MAKE THIS FUNCTION TO AS REPAY-LOAN
     /*function withdrawNft(uint256 tokenId) external returns (bool) {
         if (NftLendedByUser[msg.sender].length == 1) {
             NftsLendedByUser[msg.sender][tokenId].pop();
